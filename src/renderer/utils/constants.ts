f1ce1a50348f77c3443e5cf0fea16b3a8b492b38
mehdilouraoui <med.louraui@gmail.com>
2021-10-28 21:02:13 +0200

    🔨 create dedicated utils for renderer

---
+++ src/renderer/utils/constants.ts
@@ -0,0 +1,27 @@
+// views
+export const HISTORY = "history";
+export const ECOLOGY = "ecology";
+export const DASHBOARD = "dashboard";
+
+// dashboard menu
+export const GENERAL = "Général";
+export const VIZUALISATION = "Visualisation";
+export const ENRICHMENT = "Enrichissement";
+export const AUDIT = "Audit";
+export const REDUNDANCE = "Redondance";
+
+// cards
+export const CARD_SIMPLE = "card-simple";
+export const CARD_DOUBLE = "card-double";
+export const CARD_FULL = "card-full";
+
+// workspace
+export const BUBBLE = "bubble";
+export const START_SCREEN = "start-screen";
+export const DROPZONE = "dropzone";
+
+// home
+export const HOME = "home";
+
+// extractor
+export const ACCEPTED_EXTENSION = ".pst";
