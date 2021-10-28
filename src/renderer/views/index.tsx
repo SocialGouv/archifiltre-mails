7ea1991b25ddb90c619053cf1164d69b1f148517
mehdilouraoui <med.louraui@gmail.com>
2021-10-28 21:05:18 +0200

    🛣️ make start-screen initial route

--- src/renderer/views/index.tsx
+++ src/renderer/views/index.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 
-import { DASHBOARD, ECOLOGY, HISTORY } from "../../common/constants";
 import { useRouteContext } from "../context/RouterContext";
+import { DASHBOARD, ECOLOGY, HISTORY, START_SCREEN } from "../utils/constants";
 import { Dashboard } from "./dashboard/Dashboard";
 import { Ecology } from "./ecology/Ecology";
 import { History } from "./history/History";
+import { StartScreen } from "./start-screen/StartScreen";
 
 export const Views: React.FC = () => {
     const { route } = useRouteContext();
 
     switch (route) {
+        case START_SCREEN:
+            return <StartScreen />;
+
         case DASHBOARD:
             return <Dashboard />;
         case HISTORY:
@@ -17,6 +21,6 @@ export const Views: React.FC = () => {
         case ECOLOGY:
             return <Ecology />;
         default:
-            return <Dashboard />;
+            return <StartScreen />;
     }
 };
