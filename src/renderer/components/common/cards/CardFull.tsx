2422e50230777978c4afd0487301309fe167e11a
mehdilouraoui <med.louraui@gmail.com>
2021-10-28 20:57:01 +0200

    :lipstick: clean card logic

--- src/renderer/components/common/cards/CardFull.tsx
+++ src/renderer/components/common/cards/CardFull.tsx
@@ -1,16 +1,13 @@
 /* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from "react";
+import type { CardItemFullProps } from "src/renderer/utils/type";
 
 import style from "./Card.module.scss";
 
-interface CardFullInterface {
-    changeRoute: () => void;
-}
-
-export const CardFull: React.FC<CardFullInterface> = ({ changeRoute }) => {
+export const CardFull: React.FC<CardItemFullProps> = ({ opener }) => {
     return (
-        <div className={style["card-full"]} onClick={changeRoute}>
+        <div className={style["card-full"]} onClick={opener}>
             Card Full
         </div>
     );
