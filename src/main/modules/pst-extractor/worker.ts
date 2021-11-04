1f005514048f83b9997f29982ea491e7b9aee292
mehdilouraoui <med.louraui@gmail.com>
2021-11-04 14:19:49 +0100

    :wrench: add uuid to avoid react key error in map

--- src/main/modules/pst-extractor/worker.ts
+++ src/main/modules/pst-extractor/worker.ts
@@ -2,6 +2,7 @@ import type {
     PstContent,
     PstProgressState,
 } from "@common/modules/pst-extractor/type";
+import { randomUUID } from "crypto";
 import path from "path";
 import type { PSTFolder } from "pst-extractor";
 import { PSTFile } from "pst-extractor";
@@ -62,7 +63,7 @@ function processFolder(
     progressState: PstProgressState
 ): PstContent {
     const content: PstContent = {
-        contentSize: folder.contentCount,
+        contentSize: Math.abs(folder.contentCount) || 1,
         name: folder.displayName,
         size: folder.emailCount,
     };
@@ -86,7 +87,9 @@ function processFolder(
 
             const emailContent: PstContent = {
                 // TODO: change name
-                name: `${email.senderName} ${email.originalSubject}`,
+                name: `${email.senderName} ${
+                    email.originalSubject
+                } ${randomUUID()}`,
             };
             if (email.hasAttachments) {
                 emailContent.children = [];
@@ -96,7 +99,9 @@ function processFolder(
                     progressState.countTotal++;
                     emailContent.children.push({
                         // TODO: change name
-                        name: `Attachement: ${attachement.displayName} - ${attachement.pathname}`,
+                        name: `Attachement: ${attachement.displayName} - ${
+                            attachement.pathname
+                        } ${randomUUID()}`,
                     });
                 }
             }
