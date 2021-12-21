b4f1e59f0344689ff738e2609dc4c16a803247d8
Lilian Saget-Lethias <5783789+lsagetlethias@users.noreply.github.com>
2021-12-21 14:55:37 +0100

    fix(worker): prod and dist compat (#53)

--- src/main/worker.ts
+++ src/main/worker.ts
@@ -1,24 +1,27 @@
+import { IS_DIST_MODE, IS_PACKAGED } from "@common/config";
 import path from "path";
-import type { URL } from "url";
 import type { WorkerOptions } from "worker_threads";
 import { Worker as BaseWorker } from "worker_threads";
 
 const WORKER_BRIDGE_PATH = path.resolve(__dirname, "_worker.js");
+const PACKAGED_WORKERS_FOLDER_RESOURCE_PATH = "workers";
 
 /**
  * Worker thread wrapper for typescript workers.
  */
 export class TSWorker<TMessageValue = unknown> extends BaseWorker {
     constructor(
-        absoluteWorkerPath: URL | string,
+        mainRelativeWorkerPath: string,
         options?: Omit<WorkerOptions, "eval">
     ) {
-        super(WORKER_BRIDGE_PATH, {
+        const _workerPath = getWorkerPath(mainRelativeWorkerPath);
+        console.log(_workerPath);
+        super(_workerPath.endsWith(".js") ? _workerPath : WORKER_BRIDGE_PATH, {
             ...options,
             eval: false,
             workerData: {
                 ...options?.workerData,
-                _workerPath: absoluteWorkerPath,
+                _workerPath,
             },
         });
     }
@@ -30,3 +33,22 @@ export class TSWorker<TMessageValue = unknown> extends BaseWorker {
         super.postMessage(value);
     }
 }
+
+const getWorkerPath = (mainRelativeWorkerPath: string) => {
+    // TODO: path validator
+    if (IS_PACKAGED()) {
+        const absoluteProdWorkerPath = path.resolve(
+            process.resourcesPath,
+            PACKAGED_WORKERS_FOLDER_RESOURCE_PATH,
+            mainRelativeWorkerPath.replace(/\.ts$/gi, ".js")
+        );
+
+        return absoluteProdWorkerPath;
+    }
+    const distDevWorkerPath = path.resolve(__dirname, mainRelativeWorkerPath);
+
+    if (IS_DIST_MODE) {
+        return distDevWorkerPath.replace(/\.ts$/gi, ".js");
+    }
+    return distDevWorkerPath;
+};
