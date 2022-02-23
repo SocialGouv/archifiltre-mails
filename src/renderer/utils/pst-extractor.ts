b79a2eb394c96e69d7507eba1efa74e773423932
medlouraoui <med.louraoui@gmail.com>
2022-02-23 19:37:38 +0100

    :sparkles: add extreme dates

--- src/renderer/utils/pst-extractor.ts
+++ src/renderer/utils/pst-extractor.ts
@@ -1,7 +1,44 @@
-import type { PstContent } from "@common/modules/pst-extractor/type";
+import type {
+    PstContent,
+    PstEmail,
+    PstExtractTables,
+} from "@common/modules/pst-extractor/type";
 
 /**
  * Get all folder name from a given PST.
  */
 export const getPstFolderList = (pst: PstContent): string[] =>
     pst.children[0]!.children!.map(({ name }) => name);
+
+export interface ExtremeMails {
+    max: string;
+    min: string;
+}
+
+export const getExtremumDate = (
+    dates: PstEmail[],
+    compare: "max" | "min",
+    messageType: "receivedDate" | "sentTime"
+): string =>
+    new Date(
+        Math[compare](
+            ...dates.map((date) => new Date(date[messageType]!).getTime())
+        )
+    ).toDateString();
+
+export const getExtremeMailsDate = (
+    extractTables: PstExtractTables
+): ExtremeMails => {
+    const dates: PstEmail[] = [...Object(extractTables.emails).values()].flat();
+
+    const extremumMaxS = getExtremumDate(dates, "max", "sentTime");
+    const extremumMinS = getExtremumDate(dates, "min", "sentTime");
+
+    const extremumMaxR = getExtremumDate(dates, "max", "receivedDate");
+    const extremumMinR = getExtremumDate(dates, "min", "receivedDate");
+
+    const max = extremumMaxR > extremumMaxS ? extremumMaxR : extremumMaxS;
+    const min = extremumMinR < extremumMinS ? extremumMinR : extremumMinS;
+
+    return { max, min };
+};
