621346e45cd65eb146f4bd5fb8205fb6b607533e
Lilian Saget-Lethias <lilian.sagetlethias@gmail.com>
2022-04-26 12:22:43 +0200

    :green_heart: Remove devtools in e2e

--- tests/e2e/app.spec.ts
+++ tests/e2e/app.spec.ts
@@ -1,48 +1,16 @@
 import type { ElectronApplication, Page } from "@playwright/test";
-import { _electron as electron, expect, test } from "@playwright/test";
-import path from "path";
+import { expect, test } from "@playwright/test";
+
+import { closeApp, startApp } from "./utils/test";
 
 test.describe("App e2e", () => {
     let electronApp: ElectronApplication, win: Page;
     test.beforeEach(async () => {
-        const main = path.resolve(
-            __dirname,
-            "..",
-            "..",
-            "dist",
-            "main",
-            "main.js"
-        );
-        electronApp = await electron.launch({
-            args: [
-                // "--disable_splash_screen",
-                // "--disable-extensions",
-                // "--disable-dev-shm-usage",
-                // "--no-sandbox",
-                // "--disable-gpu",
-                // "--headless",
-                // "--disable-software-rasterizer",
-                // "--disable-setuid-sandbox",
-                main,
-            ],
-            env: {
-                DISPLAY: ":0", //TODO: input as env var
-                E2E: "true",
-                NODE_ENV: "test-e2e",
-            },
-            timeout: 120000,
-        });
-        await electronApp.evaluate(({ app }) => {
-            return app.getAppPath();
-        });
-
-        win = await electronApp.firstWindow();
-        win.on("console", console.log);
-        await win.waitForLoadState();
+        [electronApp, win] = await startApp();
     });
 
     test.afterEach(async () => {
-        await electronApp.close();
+        await closeApp(electronApp);
     });
 
     test("should render the application", async () => {
